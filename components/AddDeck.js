ce66e620a5ce0748fd1d281f2695e2945d340418
Daniel Brandt <dbbrandt@gmail.com>
2020-03-26 18:44:32 -0400

    feature: Implement Add Deck input handling.

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, KeyboardAvoidingView, TextInput, Button } from "react-native";
+import { gray } from "../utils/colors";
 
 class AddDeck extends Component {
+  state = {
+    title: ""
+  };
+
+  handleSubmit = () => {
+    const { navigate } = this.props.navigation;
+    this.setState({title: ""});
+    navigate('Home');
+  };
+
+  handleChange = (text) => {
+    this.setState({title: text});
+  };
+
   render() {
+    const { title } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Add Deck</Text>
-      </View>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <Text style={{fontSize: 20}}>Enter the name of a new flash card deck:</Text>
+        <TextInput
+          value={title}
+          onChangeText={text => this.handleChange(text)}
+          placeholder={"Deck Name"}
+          autoFocus={true}
+          maxLength={30}
+          style={styles.input}
+        />
+        <Button
+          title={"Submit"}
+          onPress={this.handleSubmit}
+          disabled={!title}
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -14,8 +43,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    margin: 20
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: gray,
+    borderWidth: 1,
+    margin: 20,
+    padding: 10
   }
+
 });
 
 export default AddDeck;
--- utils/colors.js
+++ utils/colors.js
@@ -3,3 +3,4 @@
 export const lightBlue = "#caefff";
 export const white = "#fff";
 export const blue = "#2971c3";
+export const gray = "#757575";
